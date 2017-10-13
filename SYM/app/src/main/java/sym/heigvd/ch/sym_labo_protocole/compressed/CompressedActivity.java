3d285045b1b03869b9245c6df422bfd6ab9625e2
CoolPolishGuy <wojciech.myszkorowski@heig-vd.ch>
2017-10-13 15:26:02 +0200

    initiale projet

---
+++ SYM/app/src/main/java/sym/heigvd/ch/sym_labo_protocole/compressed/CompressedActivity.java
@@ -0,0 +1,15 @@
+package sym.heigvd.ch.sym_labo_protocole.compressed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sym.heigvd.ch.sym_labo_protocole.R;
+
+public class CompressedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compressed);
+    }
+}
---
+++ SYM/app/src/main/java/sym/heigvd/ch/sym_labo_protocole/differate/DifferateActivity.java
@@ -0,0 +1,15 @@
+package sym.heigvd.ch.sym_labo_protocole.differate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sym.heigvd.ch.sym_labo_protocole.R;
+
+public class DifferateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_differate);
+    }
+}
