f184d6b4d5e17fdc4d6f90e2028e9e3dd2ac1105
Naewy <katomey@gmail.com>
2017-11-03 13:37:51 +0100

    Spinner done

--- SYM/app/src/main/java/sym/heigvd/ch/sym_labo_protocole/async/AsyncActivity.java
+++ SYM/app/src/main/java/sym/heigvd/ch/sym_labo_protocole/async/AsyncActivity.java
@@ -4,10 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import sym.heigvd.ch.sym_labo_protocole.MainActivity;
 import sym.heigvd.ch.sym_labo_protocole.R;
@@ -32,12 +36,37 @@ public class AsyncActivity extends AppCompatActivity {
         this.send = (Button) findViewById(R.id.send);
         this.spinner = (Spinner) findViewById(R.id.spinner);
 
-        toSendData.setText("{ \"nom\": \"hacker\"}");
+        // Spinner listener
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                if(spinner.getSelectedItem().toString().equals(getString(R.string.XML))) {
+                    toSendData.setText("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                        "<!DOCTYPE directorySYSTEM \"http://sym.iict.ch/directory.dtd\">\n" +
+                        "<directory />");
+                }
+                else if(spinner.getSelectedItem().toString().equals(getString(R.string.plain))) {
+                    toSendData.setText("I'm some plain text and I like it !");
+                }
+                else {
+                    toSendData.setText("{ \"test\": \"toto\"}");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         // Sender settings
         final AsyncSendRequest sender = new AsyncSendRequest();
         sender.setCommunicationEventListener(new CommunicationEventListener() {
             public boolean handleServerResponse(final String response) {
+
+                // TODO problème envoyé 2 trucs d'affilés
 
                 // Code de traitement de la réponse – dans le UI-Thread
                 AsyncActivity.this.runOnUiThread(new Runnable() {
@@ -57,25 +86,42 @@ public class AsyncActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                String spinnerText = String.valueOf(spinner.getSelectedItem());
-                String url = "http://sym.iict.ch/rest/json";
-                String contentType = "application/json";
+                String[] languageParameters = recuperateLanguage();
 
-                switch (spinnerText) {
-                    case "JSON" :
-                        url = "http://sym.iict.ch/rest/json";
-                        contentType = "application/json";
-                        break;
-                    case "XML" :
-                        // TODO
-                        break;
-                    case "Plain text" :
-                        // TODO
-                        break;
-                }
-
-                sender.execute(toSendData.getText().toString(), url, contentType);
+                sender.execute(toSendData.getText().toString(), languageParameters[0], languageParameters[1]);
             }
         });
+    }
+
+    private String[] recuperateLanguage() {
+
+        String spinnerText = String.valueOf(spinner.getSelectedItem());
+        String url;
+        String contentType;
+
+        switch (spinnerText) {
+            case "JSON" :
+                url = "http://sym.iict.ch/rest/json";
+                contentType = "application/json";
+                break;
+            case "XML" :
+                url = "http://sym.iict.ch/rest/xml";
+                contentType = "application/xml";
+                break;
+            case "Plain text" :
+                url = "http://sym.iict.ch/rest/txt";
+                contentType = "text/plain";
+                break;
+            default :
+                url = "http://sym.iict.ch/rest/json";
+                contentType = "application/json";
+                break;
+        }
+
+        String[] languageParameters = new String[2];
+        languageParameters[0] = url;
+        languageParameters[1] = contentType;
+
+        return languageParameters;
     }
 }
