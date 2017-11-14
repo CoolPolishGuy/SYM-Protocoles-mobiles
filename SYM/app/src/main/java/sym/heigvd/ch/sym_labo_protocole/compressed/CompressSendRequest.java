af3e3c0aef2d8f17f484a34299da4dc103420e09
Naewy <katomey@gmail.com>
2017-11-14 08:49:50 +0100

    Comment done for compressed, everything works !

--- SYM/app/src/main/java/sym/heigvd/ch/sym_labo_protocole/compressed/CompressSendRequest.java
+++ SYM/app/src/main/java/sym/heigvd/ch/sym_labo_protocole/compressed/CompressSendRequest.java
@@ -1,24 +1,16 @@
 package sym.heigvd.ch.sym_labo_protocole.compressed;
 
 import android.os.AsyncTask;
-import android.os.SystemClock;
-import android.text.style.AlignmentSpan;
 import android.util.Log;
-
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.logging.Level;
 import java.util.zip.*;
 
 import sym.heigvd.ch.sym_labo_protocole.utils.CommunicationEventListener;
@@ -26,15 +18,16 @@ import sym.heigvd.ch.sym_labo_protocole.utils.CommunicationEventListener;
 import static java.util.zip.Deflater.BEST_COMPRESSION;
 
 /**
- * Created by wojtek on 10/27/17.
+ * Java asynchronous activity used to send a compressed request to the server.
+ *
+ * @author Tano Iannetta, Lara Chauffoureaux, Wojciech Myszkorowski
  */
-
 public class CompressSendRequest extends AsyncTask<String, String, String> {
 
-    private CommunicationEventListener listener;
-    private static final String USER_AGENT = "Mozilla/5.0";
-    private static final String CONTENT_TYPE = "application/json";
-    private static final String END_OF_RESP = "PHP_SELF";
+    private CommunicationEventListener listener;                    // The listener to notify
+    private static final String USER_AGENT = "Mozilla/5.0";         // The user-agent header
+    private static final String CONTENT_TYPE = "application/json";  // The content-type header
+    private static final String END_OF_RESP = "PHP_SELF";           // String to find end of response
 
     @Override
     public String doInBackground(String[] arguments) {
@@ -46,9 +39,17 @@ public class CompressSendRequest extends AsyncTask<String, String, String> {
         super.onPostExecute(result);
         listener.handleServerResponse(result);
     }
+
+    /**
+     * Method sending a post request to a given url.
+     *
+     * @param request The data to send
+     * @param url     The URL where to send the request
+     * @return The server's response body if everything was ok, the error code otherwise
+     */
     private String sendRequest(String request, String url) {
 
-        String answer = null;
+        String answer = "";
 
         try {
             // Connection opening
@@ -59,12 +60,12 @@ public class CompressSendRequest extends AsyncTask<String, String, String> {
             connection.setRequestMethod("POST");
             connection.setRequestProperty("User-Agent", USER_AGENT);
             connection.setRequestProperty("Content-Type", CONTENT_TYPE);
-            connection.setRequestProperty("X-Network","CSD");
-            connection.setRequestProperty("X-Content-Encoding","deflate");
-
+            connection.setRequestProperty("X-Network", "CSD");
+            connection.setRequestProperty("X-Content-Encoding", "deflate");
             connection.setDoOutput(true);
 
             OutputStream os = connection.getOutputStream();
+
             // Write of the data & compression
             BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new DeflaterOutputStream(os, new Deflater(BEST_COMPRESSION, true))));
 
@@ -81,24 +82,22 @@ public class CompressSendRequest extends AsyncTask<String, String, String> {
                 // Reading body of the answer
                 // Decompress the bytes
                 InputStream in = connection.getInputStream();
-                in = new InflaterInputStream(in,new Inflater(true));
+                in = new InflaterInputStream(in, new Inflater(true));
                 BufferedReader incoming = new BufferedReader(new InputStreamReader(in));
 
-                String line ;
+                String line;
                 while ((line = incoming.readLine()) != null) {
-                    answer+=line;
+                    answer += line;
                 }
                 incoming.close();
-
-            }
-            else {
+            } else {
                 // Printing the response message
                 answer = connection.getResponseMessage();
             }
         } catch (Exception e) {
 
             // For debug purpose
-            Log.e("AsyncSendRequest","error in HTTP things : " + e.getMessage(), e);
+            Log.e("CompressSendRequest", "error in HTTP things : " + e.getMessage(), e);
             answer = e.getMessage();
         }
 
--- SYM/app/src/main/java/sym/heigvd/ch/sym_labo_protocole/compressed/CompressedActivity.java
+++ SYM/app/src/main/java/sym/heigvd/ch/sym_labo_protocole/compressed/CompressedActivity.java
@@ -7,14 +7,20 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import sym.heigvd.ch.sym_labo_protocole.R;
-import sym.heigvd.ch.sym_labo_protocole.async.AsyncSendRequest;
 import sym.heigvd.ch.sym_labo_protocole.utils.CommunicationEventListener;
 
+/**
+ * This activity allow the user to send requests with compressed content.
+ * The server's response contain also compressed content, thus we need to decompress it
+ * before displaying it to the user.
+ *
+ * @author Tano Iannetta, Lara Chauffoureaux, Wojciech Myszkorowki
+ */
 public class CompressedActivity extends AppCompatActivity {
 
-    private EditText receivedData;
-    private EditText toSendData;
-    private Button send;
+    private EditText receivedData;  // Edit text with received data when available
+    private EditText toSendData;    // Edit text containing the data of the user
+    private Button send;            // Button send
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,7 @@ public class CompressedActivity extends AppCompatActivity {
         this.receivedData = (EditText) findViewById(R.id.receivedData);
         this.toSendData = (EditText) findViewById(R.id.toSendData);
         this.send = (Button) findViewById(R.id.send);
+
         toSendData.setText(R.string.json_default_fill);
 
         // "Compressed" button listener
@@ -32,25 +39,26 @@ public class CompressedActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-            // Sender settings
-            final CompressSendRequest sender = new CompressSendRequest();
-            sender.setCommunicationEventListener(new CommunicationEventListener() {
-                public boolean handleServerResponse(final String response) {
+                // Sender settings
+                final CompressSendRequest sender = new CompressSendRequest();
+                sender.setCommunicationEventListener(new CommunicationEventListener() {
+                    public boolean handleServerResponse(final String response) {
 
-                    // Code de traitement de la réponse – dans le UI-Thread
-                    CompressedActivity.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            // Update view
-                            receivedData.setText(response);
-                        }
-                    });
-                    // TODO Et là on retourne quoi?
-                    return true;
-                }
-            });
+                        // Code of response treatment, as to be in the UI thread
+                        CompressedActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // Update view
+                                receivedData.setText(response);
+                            }
+                        });
+                        // TODO Et là on retourne quoi?
+                        return true;
+                    }
+                });
 
-            sender.execute(toSendData.getText().toString(), "http://sym.iict.ch/rest/json");
+                // Sending of the request
+                sender.execute(toSendData.getText().toString(), "http://sym.iict.ch/rest/json");
             }
         });
     }
